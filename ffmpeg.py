d99916de32bd317670199db6d3e43538bc0c76eb
lannguyen <18120051@student.hcmus.edu.vn>
2021-11-15 23:34:27 +0700

    get video infos

--- ffmpeg.py
+++ ffmpeg.py
@@ -18,3 +18,23 @@ def run(args,mode = 0):
         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         sout = p.stdout.readlines()
         return sout
+
+def get_video_infos(videopath):
+    args =  ['ffprobe -v quiet -print_format json -show_format -show_streams', '-i', '"'+videopath+'"']
+
+    out_string = run(args,mode=0)
+
+    infos = json.loads(out_string)
+    try:
+        fps = eval(infos['streams'][0]['avg_frame_rate'])
+        endtime = float(infos['format']['duration'])
+        width = int(infos['streams'][0]['width'])
+        height = int(infos['streams'][0]['height'])
+
+    except Exception as e:
+        fps = eval(infos['streams'][1]['r_frame_rate'])
+        endtime = float(infos['format']['duration'])
+        width = int(infos['streams'][1]['width'])
+        height = int(infos['streams'][1]['height'])
+
+    return fps,endtime,height,width
