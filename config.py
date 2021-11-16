5eccfab7245ba0af7e5464e26654c7d80fae9ff0
lannguyen <18120051@student.hcmus.edu.vn>
2021-11-16 11:25:18 +0700

    update config values

---
+++ config.py
@@ -0,0 +1,7 @@
+# Config values whether to change
+
+pixel_imgs_dir = './imgs' # Path to images of blocks
+pixel_imgs_resize = 0 # [40] resize pixel_imgs, if 0, do not resize
+output_pixel_num = 52 # [48] how many pixels in the output video'width 
+video_path = './video/bad_apple.mp4' # Path to your video file
+inverse = False # True when you want to reverse the video
